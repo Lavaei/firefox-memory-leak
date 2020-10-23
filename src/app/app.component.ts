96355c2640202b4eb5a47e75598db4746f34e364
Mostafa Levaei <twco@ymail.com>
2020-10-23 18:02:30 +0330

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls:   ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'firefox-memory-leak';
+export class AppComponent implements OnInit, AfterViewInit
+{
+  @ViewChild('video') video: ElementRef<HTMLVideoElement>;
+
+  constructor()
+  {
+  }
+
+  ngOnInit(): void
+  {
+  }
+
+  ngAfterViewInit(): void
+  {
+    this._play();
+  }
+
+  /**
+   * Force to play the video even if autoplay attribute does not work
+   * @protected
+   */
+  protected _play(): void
+  {
+    this.video.nativeElement.play();
+  }
+
+  reload(): void
+  {
+    window.location.reload();
+  }
 }
